221a5ad4e35accc772550814273d5e674443ee44
bobbydawson0702 <bobbydawson0702@gmail.com>
2024-05-05 16:11:18 -0400

    chore(frontend): add log for user profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
   const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
     useAuth0();
+
+  useEffect(() => {
+    if(user) {
+      console.log('userinfo-------------------->', user);
+    }
+  }, [user])
 
   if (isLoading) {
     return <div>Loading...</div>;
