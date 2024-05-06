1b1e0e87ec97c1064b37703058756590b59d07fc
bobbydawson0702 <bobbydawson0702@gmail.com>
2024-05-05 20:22:05 -0400

    fix(frontend): fix implement login with twitter using react-twitter-login

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,18 @@
-// src/App.js
-import React, { useEffect } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import TwitterLogin from "react-twitter-login";
 
-function App() {
-  const { isLoading, isAuthenticated, error, user,  logout, loginWithRedirect, getAccessTokenSilently, getAccessTokenWithPopup } =
-    useAuth0();
+export default () => {
+  const authHandler = (err: any, data: any) => {
+    console.log(err, data);
+  };
+  const CONSUMER_KEY = process.env.REACT_APP_TWITTER_V2_APP_KEY;
+  const CONSUMER_SECRET = process.env.REACT_APP_TWITTER_V2_APP_SECRET;
 
-  useEffect(() => {
-    if(user) {
-      console.log('userinfo-------------------->', user);
-    }
-  }, [user])
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
-
-  if (isAuthenticated) {
-    return (
-      <div>
-        Hello {user?.name}{' '}
-        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-          Log out
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <>
-      <button onClick={() => loginWithRedirect()}>      Log in with Twitter    </button>;
-      <button onClick={() => getAccessTokenSilently()}>      Log in with Twitter getAccessTokenSilently    </button>;
-      <button onClick={() => getAccessTokenWithPopup()}>      Log in with Twitter  getAccessTokenSilently   </button>;
-      </>
-    )
-  }
-}
-
-export default App;
+  return (
+    <TwitterLogin
+      authCallback={authHandler}
+      consumerKey={CONSUMER_KEY || ''}
+      consumerSecret={CONSUMER_SECRET || ''}
+    />
+  );
+};
